5626829d8574b19f9dbf64571d7f4bcc1eaa534d
jabez wekesa <wekesajabez6550@gmail.com>
2024-07-01 17:23:55 +0300

    added the recieve,returns,and annonymous function

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    const namedFunction = function() {};
+    return namedFunction;
+ }
+ function returnsAnAnonymousFunction() {
+    return function() {
+    }
+}
+ 
